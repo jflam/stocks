09df45718cd27372cce3799f70d783a43c897277
John Lam <jflam@microsoft.com>
2017-04-28 19:05:19 -0700

    Define functions that will create a dataframe that contains a portfolio

--- Stocks/script.R
+++ Stocks/script.R
@@ -39,4 +39,45 @@ library(TTR)
 symbols <- stockSymbols()
 write.csv(symbols, "symbols.csv")
 
+# Create a dataframe that contains a list of the symbols
+# in your portfolio, the date acquired / disposed, and
+# the price that you paid. 
+
+portfolio <- data.frame(
+     stock = character(), # FEE is reserved symbol for tx fees
+     action = factor(), # BUY, SELL
+     date = numeric(),
+     price = numeric()
+)
+
+portfolio <- rbind(portfolio, data.frame(
+    stock = "AMZN",
+    action = "BUY",
+    date = ISOdate(2017,1,1),
+    price = 749.23
+))
+
+# Helper functions to make it easier to enter data
+
+create_portfolio <- function() {
+    return(data.frame(
+        stock = character(), # FEE is reserved symbol for tx fees
+        action = factor(), # BUY, SELL
+        date = numeric(),
+        price = numeric()
+    ));
+}
+
+add_row <- function(df, stock, action, year, month, day, price) {
+    rbind(df, data.frame(
+        stock, action, ISOdate(year, month, day), price
+    ))
+}
+
+portfolio <- create_portfolio()
+portfolio <- add_row(portfolio, "AMZN", "BUY", 2017, 1, 1, 749.23)
+portfolio <- add_row(portfolio, "MSFT", "BUY", 2017, 3, 1, 62.52)
+
+
+portfolio <- NULL
 # Read symbols
