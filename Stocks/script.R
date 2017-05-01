cbc84de0e62fa425c14a9f0aaac9ab35df5bc4bf
John Lam <jflam@microsoft.com>
2017-05-01 16:41:16 -0700

    Completing the code that does the computations and simplifying the focus

--- Stocks/script.R
+++ Stocks/script.R
@@ -8,6 +8,9 @@ install_if_not_present <- function(name) {
     }
 }
 
+# TODO: pare this list down to a more manageable set ... the 
+# downstream dependencies are crazy here!
+
 install_if_not_present("shiny")
 install_if_not_present("ggvis")
 install_if_not_present("quantmod")
@@ -62,11 +65,78 @@ add_row <- function(df, stock, action, date, price) {
 portfolio <- create_portfolio()
 portfolio <- add_row(portfolio, "AMZN", "BUY", "2017-01-01", 749.23)
 portfolio <- add_row(portfolio, "MSFT", "BUY", "2017-03-03", 62.52)
+portfolio <- add_row(portfolio, "MSFT", "BUY", "2017-03-03", 62.52)
 
 write.csv(portfolio, "portfolio.csv", row.names = FALSE)
 
 # Read in a portfolio
 
-portfolio <- read.csv("portfolio.csv")
+starting_balance = 100000
+portfolio <- read.csv("portfolio.csv", stringsAsFactors = FALSE)
 
+# Compute a new portfolio value on a daily basis
+# Compute using min and max of dates in a column
 
+start_date <- min(portfolio$date)
+end_date <- max(portfolio$date)
+
+# Simple computation - take QQQ quotes for those days and compute
+# value on a daily basis
+
+library(quantmod)
+getSymbols("QQQ")
+
+# Compute a blended portfolio
+# 1. Get the min and max date ranges from portfolio
+# 2. Get a unique list of all of the stocks in the portfolio
+# 3. Retrieve the symbols for each of those stocks via getSymbols() API
+# 4. Compute the daily returns for each symbol between the min and the max
+# 5. Generate a daily returns dataframe that contains 
+#    a. Row for each day
+#    b. Column for each stock that contains the daily return for that day
+# 6. Compute a returns dataframe that contains 
+#    a. Rows representing each day
+#    b. Columns representing the returns from that investment for that day
+
+symbols <- unique(portfolio$stock)
+symbols[1] == "AMZN"
+symbols[2] == "MSFT"
+
+e <- new.env()
+getSymbols(symbols, env = e, from = start_date, to = end_date)
+
+# Can index
+ls(e)[1]
+
+# Can iterate using for (v in ls(e))
+
+library(quantmod)
+
+# BUGBUG: note that RTVS fails to render this correctly - we need
+# to have the date show up as the row labels. R does this correctly
+# when you just dump drAMZN to the console. RStudio also renders this
+# correctly in their table viewer
+
+drAMZN <- dailyReturn(e$AMZN)
+drMSFT <- dailyReturn(e$MSFT)
+
+# TODO: compute dailyReturns of all of the stocks and concatenate
+# them all together column-wise into a struct that contains returns by stock
+
+c <- cbind(AMZN = drAMZN, MSFT = drMSFT)
+colnames(c) <- symbols
+
+# Can now access via names or index
+head(c$AMZN)
+for (z in c) {
+    head(z)
+}
+
+# Can we read all of the symbols into a dataframe vs. side effects?
+t <- getSymbols(symbols, auto.assign = FALSE)
+
+msft = getSymbols("MSFT", auto.assign = FALSE, from = start_date, to = end_date)
+
+# Let's do the simplified version of this where you can compare a portfolio of stocks
+# all purchased on a single start date with an index vs. buy/sell lots
+# That will be an exercise left to the reader :)
