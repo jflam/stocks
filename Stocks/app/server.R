3115aceebf9803e36d6d95f9bbe7f6b2e93bb2d4
John Lam <jflam@microsoft.com>
2017-05-02 18:18:05 -0700

    Updating to use new functions in Shiny app

--- Stocks/app/server.R
+++ Stocks/app/server.R
@@ -4,18 +4,46 @@ library(highcharter)
 library(rhandsontable)
 
 starting.investment = 100000
-start_date = "2017-01-01"
+start.date = "2017-01-01"
 
-get.daily.returns <- function(symbol) {
-    return(dailyReturn(
-        Ad(
-            getSymbols(
-                toString(symbol),
-                auto.assign = FALSE,
-                from = start_date)
-            )
-        )
-    )
+create.starting.portfolio <- function() {
+    portfolio.symbols <- c("AMZN", "MSFT", "TSLA")
+    portfolio.percentage = c(0.40, 0.20, 0.40)
+    df = data.frame(
+        symbols = portfolio.symbols,
+        percentage = portfolio.percentage,
+        stringsAsFactors = FALSE)
+    return(df)
+}
+
+compute.portfolio.daily.book.value <- function(portfolio, dollars) {
+    portfolio <- cbind(portfolio, dollars = portfolio$percentage * dollars)
+    df <- NULL
+
+    compute.daily.book.value <- function(symbol, dollars) {
+
+        symbol.data <- getSymbols(
+            symbol,
+            auto.assign = FALSE,
+            from = start.date)
+
+        shares <- dollars / first(Op(symbol.data))
+        book.value <- Ad(symbol.data) * shares
+
+        if (is.null(df)) {
+            df <<- data.frame(book.value)
+        }
+        else {
+            df <<- cbind(df, data.frame(book.value))
+        }
+    }
+
+    mapply(compute.daily.book.value, portfolio$symbols, portfolio$dollars)
+
+    # Compute totals for each day
+
+    df <- cbind(df, data.frame(Total = rowSums(df)))
+    return(df)
 }
 
 function(input, output) {
@@ -26,58 +54,39 @@ function(input, output) {
 
         if (is.null(input$hot)) return()
 
-        # Compute and plot
+        # Compute the value of the portfolio and the comparison 
+        # portfolio (we do QQQ here)
 
-        DF = hot_to_r(input$hot)
-        portfolio.symbols <- DF$Stocks
-        portfolio.daily.returns <- do.call(
-            cbind,
-            lapply(
-                portfolio.symbols,
-                get.daily.returns
-            )
+        portfolio = hot_to_r(input$hot)
+
+        v$portfolio <- compute.portfolio.daily.book.value(portfolio, starting.investment)
+        v$index <- compute.portfolio.daily.book.value(
+            data.frame(
+                symbols = c("QQQ"),
+                percentage = c(1.0),
+                stringsAsFactors = FALSE
+            ),
+            starting.investment
         )
-        portfolio.proportion = DF$Fractions
-        portfolio.starting.amounts = portfolio.proportion * starting.investment
-
-        # Now let's compute the cumulative returns by summing over
-        # all of the rows in the columns
-
-        portfolio.cumulative.gains <- cumsum(portfolio.daily.returns * portfolio.starting.amounts)
-
-        # Create a new column from summing over each rows for 
-        # the daily cumulative gain for the symbol
-
-        portfolio.cumulative.gains <- cbind(portfolio.cumulative.gains,
-                                    Total = rowSums(portfolio.cumulative.gains))
-
-        v$portfolio <- portfolio.cumulative.gains$Total
-
-        QQQ.daily.returns <- get.daily.returns("QQQ")
-        QQQ.cumulative.gains <- cumsum(QQQ.daily.returns * starting.investment)
-
-        v$index <- QQQ.cumulative.gains
+        print(v$index)
     })
 
     output$chart <- renderHighchart({
         if (is.null(v$portfolio) || is.null(v$index)) return()
         highchart(type = "stock") %>%
-            hc_add_series(v$portfolio, name = "Portfolio") %>%
-            hc_add_series(v$index, name = "QQQ") %>%
+            hc_add_series(v$portfolio$Total, name = "Portfolio") %>%
+            hc_add_series(v$index$Total, name = "QQQ") %>%
             hc_add_theme(hc_theme_538())
     })
 
     output$hot <- renderRHandsontable({
         if (!is.null(input$hot)) {
-            DF = hot_to_r(input$hot)
+            portfolio = hot_to_r(input$hot)
         } else {
-            DF = data.frame(
-                Stocks = c("AMZN", "GOOG", "MSFT"),
-                Fractions = c(0.4, 0.4, 0.2),
-                stringsAsFactors = FALSE)
+            portfolio = create.starting.portfolio()
         }
 
-        rhandsontable(DF) %>%
+        rhandsontable(portfolio) %>%
             hot_table(highlightCol = TRUE, highlightRow = TRUE)
     })
 }
